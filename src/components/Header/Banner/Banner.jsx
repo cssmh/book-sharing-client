715b15fd4ed128cb1938fab9991ba7f12319b7df
momen210 <touristmomen@gmail.com>
2024-01-20 20:26:28 +0600

    banner added

--- src/components/Header/Banner/Banner.jsx
+++ src/components/Header/Banner/Banner.jsx
@@ -1,20 +1,20 @@
-import banner1 from './../../../assets/5.jpg'
-import banner2 from './../../../assets/4.jpg'
-import banner3 from './../../../assets/7.jpg'
+import banner1 from './../../../assets/1.jpeg'
+import banner2 from './../../../assets/2.jpeg'
+import banner3 from './../../../assets/3.jpeg'
 
 const Banner = () => {
   return (
 
-    <div className="carousel w-full rounded-lg">
+    <div className="carousel w-full rounded-sm">
       <div id="slide1" className="carousel-item relative w-full ">
-        <img src={banner2} className="w-full " />
+        <img src={banner1} className="w-full " />
         <div className="absolute flex  transform -translate-y-1/2 left-5 right-5 bottom-0">
           <a href="#slide3" className="btn btn-circle btn-primary mr-5">❮</a>
           <a href="#slide2" className="btn btn-circle">❯</a>
         </div>
       </div>
       <div id="slide2" className="carousel-item relative w-full rounded-lg">
-        <img src={banner1} className="w-full " />
+        <img src={banner2} className="w-full " />
         <div className="absolute flex  transform -translate-y-1/2 left-5 right-5 bottom-0">
           <a href="#slide1" className="btn btn-circle btn-primary mr-5">❮</a>
           <a href="#slide3" className="btn btn-circle">❯</a>
