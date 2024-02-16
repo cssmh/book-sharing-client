1bc323eca740dcd152b145ea42ccd1924808d9dc
momen210 <touristmomen@gmail.com>
2024-02-16 11:19:50 +0600

    loading spinner added before get data from database

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,13 +1,26 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Navbar from "../components/Header/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
+import { FallingLines } from "react-loader-spinner";
 
 const MainLayout = () => {
+  const navigationForSpinner = useNavigation();
   return (
     <div>
       <Navbar></Navbar>
       <div className="min-h-[60vh]">
-        <Outlet></Outlet>
+        {navigationForSpinner.state === "loading" ? (
+          <div className="flex justify-center">
+            <FallingLines
+              color="#4fa94d"
+              width="70"
+              visible={true}
+              ariaLabel="falling-circles-loading"
+            />
+          </div>
+        ) : (
+          <Outlet></Outlet>
+        )}
       </div>
       <Footer></Footer>
     </div>
