e089d6f5b5ffa1c3b3ebfd91ac0da06c164ef82c
momen210 <touristmomen@gmail.com>
2024-02-23 11:23:01 +0600

    green book aos updated

--- src/components/GreenBook/GreenBook.jsx
+++ src/components/GreenBook/GreenBook.jsx
@@ -1,19 +1,19 @@
 import greenBook from "../../assets/green.png";
 const GreenBook = () => {
   return (
-    <div
-      data-aos="zoom-in"
-      data-aos-duration="1000"
-      className="bg-base-200 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center py-4 space-y-6 lg:space-y-0 mb-10"
-    >
-      <div>
+    <div className="bg-base-200 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center py-4 space-y-6 lg:space-y-0 mb-10">
+      <div data-aos="zoom-in" data-aos-duration="1000">
         <img src={greenBook} alt="" />
       </div>
       <div className="space-y-2 text-center lg:text-left">
         <p className="p-2 bg-orange-500 w-24 mx-auto lg:mx-0 text-white rounded-md">
           FEATURED
         </p>
-        <h1 className="text-4xl font-semibold">
+        <h1
+          data-aos="zoom-in"
+          data-aos-duration="1000"
+          className="text-4xl font-semibold"
+        >
           FEATURED Things To Know About Green Flat Design
         </h1>
         <p>By: Farrah Whisenhunt</p>
