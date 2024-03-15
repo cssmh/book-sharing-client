1268de1885b0f683d7372fb82009a355cc23d537
momen210 <touristmomen@gmail.com>
2024-03-15 19:25:51 +0600

    popular books button style changes

--- src/pages/PopularBooks/PopularBooks.jsx
+++ src/pages/PopularBooks/PopularBooks.jsx
@@ -20,13 +20,13 @@ const PopularBooks = () => {
         </h3>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 max-w-7xl mx-auto gap-5">
-        {popularBooks?.slice(0,3).map((books) => (
+        {popularBooks?.slice(0, 3).map((books) => (
           <PopularBookCard key={books._id} getBook={books}></PopularBookCard>
         ))}
       </div>
       <div className="flex justify-center my-10">
         <Link to="/all-books">
-          <button className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+          <button className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
             Show all Books
           </button>
         </Link>
