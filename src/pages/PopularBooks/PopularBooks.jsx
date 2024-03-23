85b6962ae0fc0433ff1cae660fd8ae3fc693d669
momen210 <touristmomen@gmail.com>
2024-03-23 18:37:55 +0600

    home page popular books loader added

--- src/pages/PopularBooks/PopularBooks.jsx
+++ src/pages/PopularBooks/PopularBooks.jsx
@@ -1,15 +1,20 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { HashLoader } from "react-spinners";
 import PopularBookCard from "../PopularBookCard/PopularBookCard";
 
 const PopularBooks = () => {
   // HomePage Popular Books
   const [popularBooks, setPopularBooks] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://book-sharing-server.vercel.app/allBooks")
       .then((res) => res.json())
-      .then((data) => setPopularBooks(data));
+      .then((data) => {
+        setPopularBooks(data);
+        setIsLoading(false);
+      });
   }, []);
 
   return (
@@ -19,18 +24,29 @@ const PopularBooks = () => {
           Our Popular Books
         </h3>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 max-w-7xl mx-auto gap-5">
-        {popularBooks?.slice(0, 3).map((books) => (
-          <PopularBookCard key={books._id} getBook={books}></PopularBookCard>
-        ))}
-      </div>
-      <div className="flex justify-center my-10">
-        <Link to="/all-books">
-          <button className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-            Show all Books
-          </button>
-        </Link>
-      </div>
+      {isLoading ? (
+        <div className="flex justify-center mt-5">
+          <HashLoader color="#FB0F5A" size={36} />
+        </div>
+      ) : (
+        <div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 max-w-7xl mx-auto gap-5">
+            {popularBooks?.slice(0, 3).map((books) => (
+              <PopularBookCard
+                key={books._id}
+                getBook={books}
+              ></PopularBookCard>
+            ))}
+          </div>
+          <div className="flex justify-center my-10">
+            <Link to="/all-books">
+              <button className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                Show all Books
+              </button>
+            </Link>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
