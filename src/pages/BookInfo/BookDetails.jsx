da91e32670eca1ab9052a7cc13a8f62c39102763
momen210 <touristmomen@gmail.com>
2024-03-29 21:08:44 +0600

    book details loader added for, more books btn shown if provider added more than 1 book

--- src/pages/BookInfo/BookDetails.jsx
+++ src/pages/BookInfo/BookDetails.jsx
@@ -5,12 +5,14 @@ import swal from "sweetalert";
 import useContextHook from "../../useCustomHook/useContextHook";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { HashLoader } from "react-spinners";
 
 const BookDetails = () => {
   const { user } = useContextHook();
   const bookData = useLoaderData();
   const navigateTo = useNavigate();
   const [providerBook, setProviderBook] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const {
     _id,
@@ -28,7 +30,10 @@ const BookDetails = () => {
   useEffect(() => {
     axios
       .get(`http://localhost:5000/allbooks?email=${book_provider_email}`)
-      .then((res) => setProviderBook(res.data.result));
+      .then((res) => {
+        setProviderBook(res.data.result);
+        setIsLoading(false);
+      });
   }, [book_provider_email]);
   // for same provider book button length end
 
@@ -65,59 +70,73 @@ const BookDetails = () => {
       <Helmet>
         <title>{book_name}</title>
       </Helmet>
-      <div className="card max-w-xl mx-auto bg-amber-100 shadow-xl p-6 my-6">
-        <h2 className="text-center font-bold text-3xl italic text-blue-800">
-          Book Provider Information
-        </h2>
-        <figure className="px-10 pt-7">
-          <img
-            className="rounded-xl w-24"
-            src={book_provider_image}
-            alt="no image"
-          />
-        </figure>
-        <div className="card-body items-center text-center p-5">
-          <h2 className="card-title text-xl text-orange-500 font-bold">
-            Name : {book_provider_name}
-          </h2>
-          <p className="text-lg font-medium">Email: {book_provider_email}</p>
-          <p className="text-lg font-medium">
-            Location: <span className="text-blue-500">{location}</span>
-          </p>
-          <p className="text-lg font-medium">
-            Phone: <span className="text-green-600">{phone}</span>
-          </p>
+      {isLoading ? (
+        <div className="flex justify-center mt-5">
+          <HashLoader color="#9933FF" size={36} />
         </div>
-      </div>
-      <div className="card bg-yellow-50 shadow-xl">
-        <figure className="px-10 pt-7">
-          <img src={book_image} alt="book" className="rounded-xl w-[380px]" />
-        </figure>
-        <div className="card-body items-center text-center pt-0">
-          <h2 className="card-title text-2xl font-bold text-blue-900 pt-5">
-            {book_name}
-          </h2>
-          <p className="w-2/3 mx-auto">{description}</p>
-          <div className="card-actions mt-2">
-            <AddBookings getBookData={bookData}></AddBookings>
+      ) : (
+        <div>
+          <div className="card max-w-xl mx-auto bg-amber-100 shadow-xl p-6 my-6">
+            <h2 className="text-center font-bold text-3xl italic text-blue-800">
+              Book Provider Information
+            </h2>
+            <figure className="px-10 pt-7">
+              <img
+                className="rounded-xl w-24"
+                src={book_provider_image}
+                alt="no image"
+              />
+            </figure>
+            <div className="card-body items-center text-center p-5">
+              <h2 className="card-title text-xl text-orange-500 font-bold">
+                Name : {book_provider_name}
+              </h2>
+              <p className="text-lg font-medium">
+                Email: {book_provider_email}
+              </p>
+              <p className="text-lg font-medium">
+                Location: <span className="text-blue-500">{location}</span>
+              </p>
+              <p className="text-lg font-medium">
+                Phone: <span className="text-green-600">{phone}</span>
+              </p>
+            </div>
           </div>
-          {providerBook.length > 1 && (
-            <Link to={`/provider/${book_provider_email}`}>
-              <button className="btn btn-sm btn-success text-white mt-1">
-                Other Books Of {book_provider_name}
-              </button>
-            </Link>
-          )}
-          {user?.email == "admin@admin.com" && (
-            <button
-              onClick={() => handleDelete(_id)}
-              className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-            >
-              Delete {book_name}
-            </button>
-          )}
+          <div className="card bg-yellow-50 shadow-xl">
+            <figure className="px-10 pt-7">
+              <img
+                src={book_image}
+                alt="book"
+                className="rounded-xl w-[380px]"
+              />
+            </figure>
+            <div className="card-body items-center text-center pt-0">
+              <h2 className="card-title text-2xl font-bold text-blue-900 pt-5">
+                {book_name}
+              </h2>
+              <p className="w-2/3 mx-auto">{description}</p>
+              <div className="card-actions mt-2">
+                <AddBookings getBookData={bookData}></AddBookings>
+              </div>
+              {providerBook.length > 1 && (
+                <Link to={`/provider/${book_provider_email}`}>
+                  <button className="btn btn-sm btn-success text-white mt-1">
+                    Other Books Of {book_provider_name}
+                  </button>
+                </Link>
+              )}
+              {user?.email == "admin@admin.com" && (
+                <button
+                  onClick={() => handleDelete(_id)}
+                  className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                >
+                  Delete {book_name}
+                </button>
+              )}
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
