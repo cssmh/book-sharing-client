8eb91e9b073ad8dedcf968abba231cf94be5d12c
momen210 <touristmomen@gmail.com>
2024-03-30 23:43:16 +0600

    review component modified and assets modified

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -5,9 +5,9 @@ import Reviewer4 from "../../assets/Reviewer4.jpg";
 
 const Review = () => {
   return (
-    <div className="max-w-7xl mx-auto flex justify-center items-center my-12">
-      <div className="w-2/5">
-        <p className="text-4xl font-semibold">
+    <div className="max-w-[1200px] mx-auto flex flex-col lg:flex-row justify-center items-center gap-3 my-12">
+      <div className="lg:w-2/5 text-center lg:text-left">
+        <p className="text-[35px] font-semibold">
           Hear from Our <span className="text-green-400">Happy Authors</span>
         </p>
         <p className="text-gray-500 mt-1 font-semibold">
@@ -15,13 +15,13 @@ const Review = () => {
           Read what they have to say about our services.
         </p>
       </div>
-      <div className="w-3/5 grid grid-cols-2 gap-4">
+      <div className="lg:w-3/5 grid lg:grid-cols-2 gap-4">
         <div
           data-aos="zoom-in-up"
           data-aos-duration="1000"
           className="shadow-xl p-6"
         >
-          <p className="font-medium text-gray-500 pb-2">
+          <p className="font-medium text-gray-500 pb-2 hover:scale-105 transition-all duration-300">
             I am striving to improve the quality of services and books offered
             by this website. I am committed to attention to detail and
             excellence, aiming for unparalleled quality.
@@ -39,7 +39,7 @@ const Review = () => {
           data-aos-duration="1000"
           className="shadow-xl p-6"
         >
-          <p className="font-medium text-gray-500 pb-2">
+          <p className="font-medium text-gray-500 pb-2 hover:scale-105 transition-all duration-300">
             Working with this web has been a pleasure. Their dedication to
             customer satisfaction is truly remarkable. I highly recommend their
             services.
@@ -47,7 +47,7 @@ const Review = () => {
           <div className="flex gap-2">
             <img src={Reviewer2} className="w-12 rounded-3xl" alt="" />
             <div>
-              <p className="font-semibold">Mrinnal Johnson</p>
+              <p className="font-semibold">Sarah Johnson</p>
               <p className="text-gray-500">CEO of Heart Disease Diagnos</p>
             </div>
           </div>
@@ -57,7 +57,7 @@ const Review = () => {
           data-aos-duration="1000"
           className="shadow-xl p-6"
         >
-          <p className="font-medium text-gray-500 pb-2">
+          <p className="font-medium text-gray-500 pb-2 hover:scale-105 transition-all duration-300">
             The team at this web is not only professional but also incredibly
             friendly. They made our project a huge success and exceeded our
             expectations
@@ -75,7 +75,7 @@ const Review = () => {
           data-aos-duration="1000"
           className="shadow-xl p-6"
         >
-          <p className="font-medium text-gray-500 pb-2">
+          <p className="font-medium text-gray-500 pb-2 hover:scale-105 transition-all duration-300">
             We have been partners with this web for years, and they have
             consistently delivered top-notch solutions. They truly understand
             our business needs.
