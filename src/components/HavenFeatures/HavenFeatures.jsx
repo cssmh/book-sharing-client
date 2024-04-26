cc272651629bc957afefd5cf7bfab50c47a51fe9
momen210 <touristmomen@gmail.com>
2024-04-26 20:20:46 +0600

    profile feature of the project modal updated and make it close clicking anywhere not only close button

--- src/components/HavenFeatures/HavenFeatures.jsx
+++ src/components/HavenFeatures/HavenFeatures.jsx
@@ -46,101 +46,115 @@ const HavenFeatures = () => {
             there is regex validation for a valid phone number, ensuring only
             valid Bangladeshi phone numbers are accepted. <br />
             <br />
-            10. When a user updates their profile photo and name, these changes
+            10. On the &apos;My Added Books&apos; page, users can view the books
+            they have added. For each book, they have the option to view
+            details, update information, or delete the book. However, if a book
+            has been taken by someone and is therefore unavailable, only the
+            &apos;Details&apos; button will be displayed. <br />
+            <br />
+            11. When a user updates their profile photo and name, these changes
             will automatically reflect across all their book data, including the
-            providers name and photo. <br />
+            provider&lsquo;s name and photo. <br />
             <br />
-            11. Duplicate book entries are not allowed. <br />
+            12. Duplicate book entries are not allowed. <br />
             <br />
-            12. Users cannot collect their own books or book a book more than
-            once. <br />
+            13. Users cannot collect their own books. Each book can only be
+            collected by a user once. <br />
             <br />
-            13. The book details page includes various conditions, such as only
+            14. The book details page includes various conditions, such as only
             allowing the owner to update the book and displaying a &quot;More
             Books of That User&quot; button if the user has more than one book.
             The purchase button is hidden from the owner on the book details
             page. <br />
             <br />
-            14. Only the admin can delete books from the book details page. That
-            delete button also only shown for admin. <br />
+            15. Only the admin can delete books from the book details page, but
+            only if the book is available and has not been taken yet.
+            Additionally, it mentions that the delete button is only visible to
+            the admin. <br />
             <br />
-            15. If the book image field is left empty when adding a book, a
+            16. If the book image field is left empty when adding a book, a
             default photo will be added. When updating book details, the photo
             URL field will be emptied if it contains the default photo URL.{" "}
             <br />
             <br />
-            16. A default photo is provided for new users during sign-up,
+            17. A default photo is provided for new users during sign-up,
             following the same conditions as updating book photos. <br />
             <br />
-            17. Added a &quot;View Profile&quot; page. Users have the option to
+            18. Added a &quot;View Profile&quot; page. Users have the option to
             update their username and photo. If a user leaves the image URL
             field empty, the default photo will be set, and the field will
             remain empty each time. The URL will only be displayed if the user
             sets any photo. <br />
             <br />
-            18. &quot;My Booking&quot; displays the books the user has booked to
+            19. &quot;My Booking&quot; displays the books the user has booked to
             receive, while &quot;My Pending&quot; indicates if any user has
-            booked any of the users books. <br />
+            booked any of the user&lsquo;s books. <br />
             <br />
-            19. While booking a book, the status will display as
+            20. While booking a book, the status will display as
             &quot;Pending.&quot; Only the book owner can change it to
             &quot;Process&quot; or &quot;Completed.&quot; <br />
             <br />
-            20. Added React Spinners, HashLoader, and other awesome React
+            21. While adding a book, a default value of &lsquo;available&rsquo;
+            will be assigned to the book status on the server. When a user
+            requests a book and the owner sets it from &lsquo;pending&rsquo; to
+            &lsquo;completed&rsquo;, the book status will change to
+            &lsquo;unavailable&rsquo;. Afterward, other users won&apos;t be able
+            to collect that book, and it will indicate that the book has already
+            been taken. <br />
+            <br />
+            22. Added React Spinners, HashLoader, and other awesome React
             packages. <br />
             <br />
-            21. The counting component on the homepage dynamically displays the
+            23. The counting component on the homepage dynamically displays the
             total number of users and book providers. <br />
             <br />
-            22. Implemented JSON Web Token (JWT) for authorization. Only
+            24. Implemented JSON Web Token (JWT) for authorization. Only
             logged-in users can access their own data, not others. <br />
             <br />
-            23. Used interceptors to automatically log out users and redirect
+            25. Used interceptors to automatically log out users and redirect
             them to the login page if the token expires or encounters any
             issues. <br />
             <br />
-            24. Created an admin dashboard accessible only to admin@***.com.
+            26. Created an admin dashboard accessible only to admin@***.com.
             Other users attempting to access this route will be automatically
             redirected to the homepage. <br />
             <br />
-            25. The &quot;More Books of a User&quot; page and the Admin Page
+            27. The &quot;More Books of a User&quot; page and the Admin Page
             display book details. If the book count is 0 or 1, the page title
             will be singular (&quot;Book&quot;). If there are more than one
             book, it will display in plural form
             (&quot;Books&quot;/&quot;Providers&quot;/&quot;Bookings&quot;).{" "}
             <br />
             <br />
-            26. In the admin dashboard, the admin can view the total number of
+            28. In the admin dashboard, the admin can view the total number of
             books, total number of book providers, and provider emails, along
             with the count of books each user has added. Each count number
             serves as a link to see the books added by that user. Additionally,
             the admin can access total bookings and their details. <br />
             <br />
-            27. Access to all bookings from the database is restricted to
+            29. Access to all bookings from the database is restricted to
             authorized admins only. Even with a valid token and user
             credentials, users cannot access this data. The server implements an
             admin email condition to restrict access to admin emails only.{" "}
             <br />
             <br />
-            28. The admin can delete any bookings, including completed bookings,
+            30. The admin can delete any bookings, including completed bookings,
             or all bookings at once. <br />
             <br />
-            29. Users cannot update other users book data using the book ID from
-            the URL. Provider validation is applied both server-side and
+            31. Users cannot update other users&apos; book data using the book
+            ID from the URL. Provider validation is applied both server-side and
             client-side. Postman requests will not work due to JWT token
             verification during updates. <br />
             <br />
-            30. Users cannot delete any book using Postman with the book ID from
+            32. Users cannot delete any book using Postman with the book ID from
             the book details URL. Provider email and JWT verification are
-            required for deletion. Only the admin and the book owner can delete
-            a book. <br />
-            <br />
-            31. Similarly, access to all bookings for the admin is restricted.
-            Only the admin can delete bookings. Postman requests will not work
-            due to JWT verification and query conditions restricting access to
-            admin emails.
+            required
           </p>
         </div>
+        <div
+          className="modal-backdrop"
+          onClick={() => document.getElementById("my_modal_4").close()}
+        ></div>
       </dialog>
     </>
   );
